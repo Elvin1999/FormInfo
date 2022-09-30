66f9dae95a19b0e1b2ce5eb7b3be181307971ec1
Elvin Camalzade <e.camalzade@itstep.edu.az>
2022-09-30 16:07:25 +0400

    Add project files.

---
+++ WindowsFormsApp7/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+
+        EventHandler<EventArgs> handler;
+        public string Data
+        {
+            get { return label1.Text; }
+            set { label1.Text = value; }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            handler = new EventHandler<EventArgs>((o, e) =>
+              {
+                  label1.Text=(o as TextBox).Text;
+              });
+
+            // 2 qayda var
+            // from parent to child
+            // by constructor
+            // by property
+            // showdialog overriding
+            // from child to parent
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //var data = textBox1.Text;
+            //Form2 form2 = new Form2(data);
+            //this.Hide();
+            //if (form2.ShowDialog() == DialogResult.Cancel)
+            //{
+            //    this.Show();
+            //}
+
+            form2.Data = textBox1.Text;
+
+        }
+
+       ////Form2 form2 = new Form2();
+       ////private void button2_Click(object sender, EventArgs e)
+       ////{
+       ////    form2.Show();
+       ////}
+
+
+
+        Form2 form2 = new Form2();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //form2.ShowDialog(new User
+            //{
+            //     Username="Elvin123",
+            //     Password="Elvin123"
+            //});
+            form2.MyHandler = handler;
+            form2.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Data = form2.Data;
+        }
+    }
+}
---
+++ WindowsFormsApp7/Form2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form2 : Form
+    {
+
+        /////by constructor version 1
+        //public Form2(string data)
+        //{
+        //    InitializeComponent();
+        //    label1.Text = data; 
+        //}
+        public EventHandler<EventArgs> MyHandler { get; set; }
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        
+        public string Data
+        {
+            get { return label1.Text; }
+            set { label1.Text = value; }
+        }
+
+        public DialogResult ShowDialog(User user)
+        {
+            label1.Text = $"{user.Username} - {user.Password}";
+            return ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Data = textBox1.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            MyHandler.Invoke(sender, e);
+        }
+    }
+}
